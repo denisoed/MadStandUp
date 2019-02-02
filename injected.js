5be990ae9bcf16e19fc94c4bcd83de89a23aabce
Denisoed <denisod93@gmail.com>
2019-02-02 10:49:30 +0600

    Rewrote the functions of creating stand-ups

--- injected.js
+++ injected.js
@@ -88,77 +88,17 @@ $(document).ready(function() {
             return today.getUTCFullYear() + '-' + ('0' + (today.getUTCMonth() + 1)).slice(-2) + '-' + ('0' + (today.getDate() - 1)).slice(-2);
         }
     }
-    
-    function get_yesterday_issues_with_worklogs() {
-        return new Promise(resolve => {
-            var theUrl = serverUrl + '/rest/api/2/search?jql=worklogDate=' + '"' + get_yesterday() + '"' + ' AND worklogAuthor=currentuser()&fields=worklog&maxResults';
-            httpGet('GET', theUrl).then(function(data) {
-                var issuesWithLogs = JSON.parse(data.target.response);
-                get_worklogs_from_issues(issuesWithLogs['issues']).then(function(data) {
-                    resolve(data);
-                });
-            }, function (e) {
-                alert(e);
-            });
-        });
-    }
-
-    function get_worklogs_from_issues(issues) {
-        return new Promise(async resolve => {
-            var comments = [];
-            issues.forEach(function(issue) {
-                get_worklogs_comments_from_issue(issue['key']).then(function(comment) {
-                    comments.push(comment);
-                });
-            });
-            setTimeout(() => {
-                resolve(comments);
-            }, 2000);
-        });
-    }
-
-    function get_worklogs_comments_from_issue(issueKey) {
-        var theUrl = serverUrl + '/rest/api/2/issue/' + issueKey + '/worklog';
-        var issueLink = serverUrl + '/browse/' + issueKey;
-        return new Promise(resolve => {
-            httpGet('GET', theUrl).then(function (data) {
-                var r = JSON.parse(data.target.response);
-                get_comments(r).then(function(comment) {
-                    var commentObj = {
-                        texts: comment,
-                        issueLink: issueLink
-                    }
-                    resolve(commentObj);
-                });
-            }, function (e) {
-                alert('Error: ' + e);
-            });
-        });
-    }
-
-    function get_comments(worklogs) {
-        var issuesWorklogList = worklogs['worklogs'];
-        return new Promise(resolve => {
-            var array = [];
-            issuesWorklogList.forEach(function (log) {
-                if (log['created'].slice(0, 10) == get_yesterday() &&
-                    userInfo['name'] == log['author']['name']) {
-                    array.push(log['comment']);
-                    resolve(array);
-                }
-            });
-        });
-    }
 
     function showStandUp(arrayText) {
-        var texts = arrayText;
+        
+        var texts = arrayText.reverse();
         var yesterday = '';
         for (let i = 0; i < texts.length; i++) {
             var comment = '';
-            for (let j = 0; j < texts[i]['texts'].length; j++) {
-                comment += texts[i]['texts'][j] + ' ';
+            for (let j = 0; j < texts[i]['comments'].length; j++) {
+                comment += texts[i]['comments'][j] + ' ';
             }
-            yesterday += '- ' + comment.trim() + ' ' + texts[i]['issueLink'] + '\n';
+            yesterday += '- ' + comment.trim() + ' ' + texts[i]['link'] + '\n';
         }
         $('#standup-text').text(
             'Доброе утро! @comedian\n\n*Вчера*\n' + yesterday +
@@ -168,10 +108,86 @@ $(document).ready(function() {
     }
 
     async function generateStandUp() {
-        await get_yesterday_issues_with_worklogs().then(function(data) {
-            showStandUp(data);
-        });
+        var issues = await get_issues_with_worklogs();
+        var keys = await get_issues_keys(issues);
+        var worklogs = await get_worklogs_comments_from_issues(keys);
+        var comments = await get_currentUser_comments(worklogs);
+        var d = await done();
+        showStandUp(comments);
     }
+
+    function get_issues_with_worklogs() {
+        return new Promise(resolve => {
+            var theUrl = serverUrl + '/rest/api/2/search?jql=worklogDate=' + '"' + get_yesterday() + '"' + ' AND worklogAuthor=currentuser()&fields=worklog&maxResults';
+            httpGet('GET', theUrl).then(function(data) {
+                var issuesWithLogs = JSON.parse(data.target.response);
+                resolve(issuesWithLogs['issues']);
+            }, function (e) {
+                alert(e);
+            });
+        });
+    };
+
+    function get_issues_keys(issues) {
+        return new Promise(resolve => {
+            var comments = [];
+            issues.forEach(function (issue) {
+                comments.push(issue['key']);
+                resolve(comments);
+            });
+        });
+    };
+
+    function get_worklogs_comments_from_issues(issueKey) {
+        return new Promise(resolve => {
+            var comments = [];
+            issueKey.forEach(function (key) {
+                var theUrl = serverUrl + '/rest/api/2/issue/' + key + '/worklog';               
+                var issueLink = serverUrl + '/browse/' + key;
+                $.ajax({
+                    url: theUrl,
+                    async: false,
+                    success: function (data) {
+                        var commentObj = {
+                            comments: data['worklogs'],
+                            link: issueLink
+                        }
+                        comments.push(commentObj);
+                        resolve(comments);
+                    }
+                });
+            });
+        });
+    };
+
+    function get_currentUser_comments(listLogs) {
+        return new Promise(resolve => {
+            var array = [];
+            listLogs.forEach(logs => {
+                var obj = {
+                    comments: [],
+                    link: logs.link
+                }
+                logs.comments.forEach(function (log) {
+                    if (log['created'].slice(0, 10) == get_yesterday() &&
+                        userInfo['name'] == log['author']['name']) {
+                        obj.comments.push(log['comment']);
+                    }
+                });
+                array.push(obj);
+                resolve(array);
+            });
+        });
+    };
+    
+    function done() {
+        return new Promise(resolve => {
+            setTimeout(function () {
+                resolve('DONE!');
+            }, 0);
+        });
+    };
+
     // ---------- END: Generate StandUp ---------- //
 
     // ------------ Main Function ----------- //
